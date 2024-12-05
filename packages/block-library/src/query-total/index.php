2f59c85cb7abab3fa63ee9bc27d0754bbecc13b5
sarthaknagoshe2002 <sarthaknagoshe2002@gmail.com>
2024-12-05 22:00:23 +0530

    Fix: removed EOL chars

--- packages/block-library/src/query-total/index.php
+++ packages/block-library/src/query-total/index.php
@@ -7,7 +7,7 @@
 
 /**
  * Renders the `query-total` block on the server.
- * 
+ *
  * @since 6.7.0
  *
  * @param array    $attributes Block attributes.
@@ -72,7 +72,7 @@ function render_block_core_query_total( $attributes, $content, $block ) {
 
 /**
  * Registers the `query-total` block.
- * 
+ *
  * @since 6.7.0
  */
 function register_block_core_query_total() {
