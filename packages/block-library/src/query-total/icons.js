e284b58f1fa97875b9f2ead87013f222170eff60
sarthaknagoshe2002 <sarthaknagoshe2002@gmail.com>
2024-12-10 14:08:13 +0530

    Fix: put the block icon in a box

--- packages/block-library/src/query-total/icons.js
+++ packages/block-library/src/query-total/icons.js
@@ -29,4 +29,15 @@ export const displayingResults = (
 	</SVG>
 );
 
-export const queryTotal = <span>&#931;</span>;
+export const queryTotal = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<Path d="M18 4H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2Zm.5 14c0 .3-.2.5-.5.5H6c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h12c.3 0 .5.2.5.5v12Zm-7-6-4.1 5h8.8v-3h-1.5v1.5h-4.2l2.9-3.5-2.9-3.5h4.2V10h1.5V7H7.4l4.1 5Z" />
+	</SVG>
+);
