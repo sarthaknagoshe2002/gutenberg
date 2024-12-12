7775dcf5f705b17a37cb39ca7b9b0519a21e9b34
sarthaknagoshe2002 <sarthaknagoshe2002@gmail.com>
2024-12-13 02:47:32 +0530

    Feat: revamped with picker component

--- packages/format-library/src/time/index.js
+++ packages/format-library/src/time/index.js
@@ -1,15 +1,18 @@
 /**
- * External dependencies
- */
-import { gmdate, strtotime } from 'locutus/php/datetime';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { toggleFormat } from '@wordpress/rich-text';
+import {
+	DateTimePicker,
+	Popover,
+	Button,
+	__experimentalHStack as HStack,
+} from '@wordpress/components';
 import { RichTextToolbarButton } from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
+import { removeFormat, applyFormat, useAnchor } from '@wordpress/rich-text';
 import { postDate as icon } from '@wordpress/icons';
+import { dateI18n, getSettings } from '@wordpress/date';
 
 const name = 'core/time';
 const title = __( 'Time' );
@@ -19,69 +22,95 @@ export const time = {
 	title,
 	tagName: 'time',
 	className: null,
+	edit: Edit,
 	attributes: {
 		datetime: 'datetime',
 	},
-	edit( { isActive, value, onChange, onFocus } ) {
-		function onClick() {
-			const dateDescription = value.text
-				.slice( value.start, value.end )
-				.trim();
-
-			// Exit early if no selection or format is already active
-			if ( ! dateDescription || isActive ) {
-				onChange(
-					toggleFormat( value, {
-						type: name,
-					} )
-				);
-				return;
-			}
-
-			// Clean up the date string
-			const dateCleaned = dateDescription
-				.replace( 'at ', '' ) // Remove "at"
-				.replace( 'UTC', 'GMT' ); // Replace "UTC" with "GMT"
-
-			// Parse the date string using strtotime
-			const timestamp = strtotime( dateCleaned );
-
-			// If parsing fails, toggle format without enhancement
-			if ( ! timestamp ) {
-				onChange(
-					toggleFormat( value, {
-						type: name,
-					} )
-				);
-				return;
-			}
-
-			// Format the datetime attributes using gmdate
-			const datetime = gmdate( 'c', timestamp ); // ISO 8601 format
-
-			// Apply the format with parsed datetime attributes
-			onChange(
-				toggleFormat( value, {
-					type: name,
-					attributes: {
-						datetime,
-					},
-				} )
-			);
-
-			onFocus();
-		}
-
-		return (
-			<>
-				<RichTextToolbarButton
-					icon={ icon }
-					title={ title }
-					onClick={ onClick }
-					isActive={ isActive }
-					role="menuitemcheckbox"
-				/>
-			</>
-		);
-	},
 };
+
+function Edit( { isActive, value, onChange, contentRef } ) {
+	const [ date, setDate ] = useState( new Date() );
+	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
+	const togglePopover = () => setIsPopoverVisible( ( state ) => ! state );
+	const dateTimeSettings = getSettings();
+
+	const applyDate = () => {
+		const formattedDate = dateI18n( 'c', date );
+		const attributes = {
+			datetime: formattedDate,
+		};
+		onChange(
+			applyFormat( value, {
+				type: name,
+				attributes,
+			} )
+		);
+		setIsPopoverVisible( false );
+	};
+
+	return (
+		<>
+			<RichTextToolbarButton
+				icon={ icon }
+				label={ title }
+				title={ title }
+				onClick={ () => {
+					if ( isActive ) {
+						onChange( removeFormat( value, name ) );
+					} else {
+						togglePopover();
+					}
+				} }
+				isActive={ isActive }
+				role="menuitemcheckbox"
+			/>
+			{ isPopoverVisible && (
+				<InlineDateUI
+					contentRef={ contentRef }
+					onClose={ togglePopover }
+					onApply={ applyDate }
+					date={ date }
+					onChangeDate={ setDate }
+					dateTimeSettings={ dateTimeSettings }
+				/>
+			) }
+		</>
+	);
+}
+
+function InlineDateUI( {
+	contentRef,
+	onClose,
+	onApply,
+	date,
+	onChangeDate,
+	dateTimeSettings,
+} ) {
+	const popoverAnchor = useAnchor( {
+		editableContentElement: contentRef.current,
+		settings: time,
+	} );
+
+	return (
+		<Popover
+			className="block-editor-format-toolbar__date-popover"
+			anchor={ popoverAnchor }
+			onClose={ onClose }
+		>
+			<DateTimePicker
+				is12Hour={ dateTimeSettings.formats.time }
+				currentDate={ date }
+				onChange={ onChangeDate }
+				startOfWeek={ dateTimeSettings.l10n.startOfWeek }
+			/>
+			<HStack alignment="right">
+				<Button
+					__next40pxDefaultSize
+					variant="primary"
+					text={ __( 'Apply' ) }
+					onClick={ onApply }
+				/>
+			</HStack>
+		</Popover>
+	);
+}
